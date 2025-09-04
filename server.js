f92991916653542f88bd8a8293fed6575e3d1664
A.M.Cornelissen <cornelissenam7@gmail.com>
2025-09-04 12:21:04 +0200

    Initial backend

---
+++ server.js
@@ -0,0 +1,1 @@
+﻿import express from "express";import cors from "cors";import bodyParser from "body-parser";const app=express();app.use(cors());app.use(bodyParser.json());const PORT=process.env.PORT||4000;app.get("/health",(_req,res)=>res.json({ok:true}));app.listen(PORT,()=>console.log(`Backend running on http://localhost:${PORT}`));
